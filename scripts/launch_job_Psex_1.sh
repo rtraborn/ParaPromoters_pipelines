48a81516f06507707d207e76465630c953ff79f6
rtraborn <rtraborn@asu.edu>
2020-08-05 20:44:18 -0400

    Added new samples (Psex) and job scripts.

---
+++ scripts/launch_job_Psex_1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#PBS -N PsexSTRIPE_job_rep_1
+#PBS -k o
+#PBS -l nodes=1:ppn=16,vmem=64gb
+#PBS -l walltime=8:00:00
+
+myDir=/N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Paramecium/STRIPES
+sampleFolder=Psex_1
+
+module load singularity
+
+cd $myDir
+
+source /N/dc2/scratch/rtraborn/myDevel/GoSTRIPES/bin/xworkStripes -b /N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Paramecium/STRIPES -i /N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Paramecium/STRIPES/gostripes.simg
+
+echo "Launching job"
+
+cd $myDir/$sampleFolder
+
+$rws make -n
+
+$rws make
+
+echo "Job complete"
