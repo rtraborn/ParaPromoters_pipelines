7406091749838858cf0d893900d2598d54a3f555
rtraborn <rtraborn@asu.edu>
2020-08-07 12:47:54 -0400

    Extended wall times for Pquad jobs.

--- scripts/launch_job_Pquad_2.sh
+++ scripts/launch_job_Pquad_2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#PBS -N PquadSTRIPE_job_rep_2
+#PBS -N Pquad_job_rep2
 #PBS -k o
 #PBS -l nodes=1:ppn=16,vmem=64gb
-#PBS -l walltime=8:00:00
+#PBS -l walltime=14:00:00
 
 myDir=/N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Paramecium/STRIPES
 sampleFolder=Pquad_2
--- scripts/launch_job_Pquad_3.sh
+++ scripts/launch_job_Pquad_3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#PBS -N PquadSTRIPE_job_rep_3
+#PBS -N Pquad_rep3
 #PBS -k o
 #PBS -l nodes=1:ppn=16,vmem=64gb
-#PBS -l walltime=8:00:00
+#PBS -l walltime=14:00:00
 
 myDir=/N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Paramecium/STRIPES
 sampleFolder=Pquad_3
