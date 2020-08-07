b9f23928f56909cdec5d06cd9cc638b3a30d39a4
R. Taylor Raborn <rtraborn@asu.edu>
2020-08-07 13:21:17 -0700

    Added an additional Pprim job.

---
+++ scripts/launch_job_Pprim_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#PBS -N Pprim_rep_2
+#PBS -k o
+#PBS -l nodes=1:ppn=16,vmem=64gb
+#PBS -l walltime=14:00:00
+
+myDir=/N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Paramecium/STRIPES
+sampleFolder=Pprim_2
+
+module load singularity
+
+cd $myDir
+
+source /N/dc2/scratch/rtraborn/myDevel/GoSTRIPES/bin/xworkStripes -b /N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Paramecium/STRIPES -i /N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Paramecium/STRIPES/gostripes.simg
+
+echo "Launching job"
+
+cd $myDir/$sampleFolder
+
+$rws make -n
+
+$rws make
+
+echo "Job complete"
